d64b305ce09ed74ce3ff06df20d600ebfa819460
wakanamake <waka.namake@gmail.com>
2023-02-15 17:20:28 +0900

    show latency

--- udp_recv.py
+++ udp_recv.py
@@ -1,5 +1,8 @@
 import socket
 import sys
+import json
+import time
+import statistics
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5999
@@ -16,9 +19,25 @@ print(f"Listen on: {UDP_PORT}")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+latencies = []
+
 while True:
 	data, addr = sock.recvfrom(4096)
-	# DEBUG
-	#print("Rcv from {}:{}".format(addr, data.decode()))
+
+	json_data = json.loads(data.decode())
+
+	current_time = time.time()
+	sent_time = json_data['timestamp']
+
+	latency = current_time - sent_time
+	latencies.append(latency)
+
+	min_latency = min(latencies)
+	avg_latency = statistics.mean(latencies)
+	max_latency = max(latencies)
+
+	print(f"Minimum latency: {min_latency:.6f}, Average latency: {avg_latency:.6f}, Maximum latency: {max_latency:.6f}", end="\r")
+
+	#print("Rcv from {}:{}".format(addr, latency))
 
 sock.close()
